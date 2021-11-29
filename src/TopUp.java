875328ef9f605332569fd0406ea2706366e0b178
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-11-29 12:20:10 +0300

    Added TopUp.java

---
+++ src/TopUp.java
@@ -0,0 +1,16 @@
+public class TopUp {
+    public static void main(String[] args) {
+        int balanceClient = 100;
+        int topUp = 1100;
+        int totalSum = balanceClient + topUp;
+        System.out.println("Баланс после пополнения " + totalSum  + " рублей.");
+        int bonus = topUp / 100 / 10;
+        if (topUp < 1000) {
+            bonus = 0;
+        }
+        System.out.println("Ваш бонус " + bonus);
+        System.out.println("Итоговая сумма " + (totalSum + bonus) + " рублей.");
+    }
+}
+
+
